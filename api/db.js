5ad5b6c760ca00fd194dd635a97cb4939a395232
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-08-09 10:58:47 +0530

    Final backend

--- api/db.js
+++ api/db.js
@@ -7,7 +7,8 @@ const con = mysql2.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
 });
 
 con.connect(function (err) {
